19d303e4e0ea3de400a0e51cf849a45e614e483a
Wilbert <xuwilbert@gmail.com>
2019-05-15 12:46:11 +0700

    Initial Commit

---
+++ WilbertGoodMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WilbertGoodMan
+//
+//  Created by Wilbert xu on 15/05/19.
+//  Copyright © 2019 AppleDeveloperAcademyBinus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
